05e8c2ab0d5a4ec05b93e18d3deccca23b386ade
Nelli <nellichka0801@yandex.ru>
2022-10-24 10:10:57 +1200

    вторая задача

--- Task-2/Program.cs
+++ Task-2/Program.cs
@@ -5,13 +5,58 @@
 //78 -> третьей цифры нет
 //32679 -> 6
 
+//1. Вводим число
+//2. Проверяем число <1000
+//3. Меньше тысячи Выводим остаток
+
 Console.Write("Введи число: ");
-int Number = Convert.ToInt32(Console.ReadLine());
-string NumberText = Convert.ToString(Number);
-if (NumberText.Length > 2)
+int number = int.Parse(Console.ReadLine()!);
+if (number<100)
 {
-  Console.WriteLine("третья цифра  " + NumberText[2]);
+  Console.WriteLine("Третьей цифры в числе нет");
 }
-else {
-  Console.WriteLine("третьей цифры нет");
+else
+{
+  int threeDigit(int number)
+    {
+      while(number>=1000)
+      {
+        number=number/10;
+      }
+      return number%10;
+    }
+   
+Console.WriteLine("третья цифра  " + threeDigit(number));
 }
+
+
+//int remainsDigit(int number);
+//while (number>=1000);
+//{
+ // number = number/10;
+ // return number;
+//}
+//{
+ // number = number%10;
+ // return number;
+//}
+//Console.WriteLine(remainsDigit(number));
+
+//int RemainsDigit = remainsDigit(number);
+//if (RemainsDigit>0)
+//{
+  //
+//}
+//else
+//{
+  //Console.WriteLine("третьей цифры нет");
+//}
+
+
+//}
+//else {
+//  Console.WriteLine("третьей цифры нет");
+//}
+
+//
+//RemainsDigit(number);
